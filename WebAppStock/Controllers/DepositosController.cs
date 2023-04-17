cf19e96d087eaaa986bb23a009f8b3801114e97f
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-04-16 23:20:57 -0300

    first commit

---
+++ WebAppStock/Controllers/DepositosController.cs
@@ -0,0 +1,15 @@
+﻿using CodigoComun.Negocio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppStock.Controllers
+{
+    public class DepositosController : Controller
+    {
+        public IActionResult Index()
+        {
+            DepositoServices depositoServices = new DepositoServices();
+            var deposito = depositoServices.depositoPorId(1);
+            return View(deposito);
+        }
+    }
+}
---
+++ WebAppStock/Controllers/StocksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppStock.Controllers
+{
+    public class StocksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
