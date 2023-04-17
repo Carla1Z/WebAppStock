a32537aefd4371acb48ed55a11038c827728594d
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-04-17 14:16:53 -0300

    nombre de articulo y deposito en stock

--- WebAppStock/Controllers/StocksController.cs
+++ WebAppStock/Controllers/StocksController.cs
@@ -11,6 +11,23 @@ namespace WebAppStock.Controllers
             StockServices stockServices = new StockServices();
             var stocks = stockServices.TodosLosStocks();
 
+            // Obtener los nombres de los artículos
+            var articulosIds = stocks.Select(s => s.IdArticulo).Distinct().ToList();
+            var articuloService = new Articulo();
+            var articulos = articulosIds.Select(id => articuloService.GetArticuloPorId((int)id));
+
+            // Obtener los nombres de los depósitos
+            var depositosIds = stocks.Select(s => s.IdDeposito).Distinct().ToList();
+            var depositoService = new DepositoServices();
+            var depositos = depositosIds.Select(id => depositoService.depositoPorId((int)id));
+
+            // Agregar los nombres de los artículos y depósitos a cada stock
+            foreach (var stock in stocks)
+            {
+                stock.Articulos = articulos.FirstOrDefault(a => a.Id == stock.IdArticulo);
+                stock.IdDepositoNavigation = depositos.FirstOrDefault(d => d.Id == stock.IdDeposito);
+            }
+
             return View(stocks);
         }
 
