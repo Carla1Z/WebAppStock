ef8f4077b38dcae422b79d27cce99d6d4ff61757
Carla1Z <carla_sabrina_zarate@hotmail.com>
2023-05-05 00:58:31 -0300

    deposito

--- WebApiStock/Controllers/DepositosController.cs
+++ WebApiStock/Controllers/DepositosController.cs
@@ -9,5 +9,49 @@ namespace WebApiStock.Controllers
 	[ApiController]
 	public class DepositosController : ControllerBase
 	{
+		private DepositoServices services = new DepositoServices();
+
+		[HttpPost]
+		public IActionResult Post(DepositoDTO depositoDTO)
+		{
+			DepositoServices services = new DepositoServices();
+			depositoDTO = services.AgregarDeposito(depositoDTO);
+			return Ok(depositoDTO);
+		}
+
+		[HttpDelete]
+		public IActionResult Delete(int Id)
+		{
+			DepositoDTO eliminarDeposito = services.EliminarDepositoSeleccionado(Id);
+			return Ok(eliminarDeposito);
+		}
+
+		[HttpPost]
+		[Route("{Id}")]
+		public IActionResult Edit(DepositoDTO Id)
+		{
+			DepositoDTO editar = services.ModificarDeposito(Id);
+			return Ok(editar);
+		}
+
+		[HttpGet]
+		public IActionResult Get()
+		{
+			List<DepositoDTO> depositoDTOs = services.TodosLosDepositos();
+			return Ok(depositoDTOs);
+		}
+
+		[HttpGet]
+		[Route("{Id}")]
+		public IActionResult Get(int Id)
+		{
+			DepositoDTO depositoDTO = services.depositoPorId(Id);
+
+			return Ok(depositoDTO);
+		}
+
+
+
+
 	}
 }
